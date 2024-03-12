ff5a2531db2cdd0db593047977571a59c5b4bba5
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2024-03-12 22:37:10 +0900

    Remove boilerplate spec

--- spec/activerecord/originator_spec.rb
+++ spec/activerecord/originator_spec.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe ActiveRecord::Originator do
-  it "has a version number" do
-    expect(ActiveRecord::Originator::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  pending
 end
