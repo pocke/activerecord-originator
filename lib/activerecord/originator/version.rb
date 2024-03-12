7a26dad9ab17f2bbbc044acfb536ac3511911a17
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2024-03-12 21:25:35 +0900

    bunlde gem

---
+++ lib/activerecord/originator/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Activerecord
+  module Originator
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/activerecord/originator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activerecord::Originator do
+  it "has a version number" do
+    expect(Activerecord::Originator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
