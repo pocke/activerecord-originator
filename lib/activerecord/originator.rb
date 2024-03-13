e8e1ae96df7c34caf683d56e1d16133c6b7e88ab
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2024-03-13 19:58:14 +0900

    Prepend ArelNodeExtension each classes
    It is necessary because some subclasses defines `initialize` and they
    does not call `super`

--- lib/activerecord/originator.rb
+++ lib/activerecord/originator.rb
@@ -2,6 +2,7 @@
 
 require 'active_record'
 require 'active_support/core_ext/module/attribute_accessors'
+require 'active_support/core_ext/class/subclasses'
 
 require_relative "originator/version"
 
@@ -9,7 +10,9 @@ require_relative "originator/arel_node_extension"
 require_relative "originator/arel_visitor_extension"
 require_relative "originator/collector_proxy"
 
-Arel::Nodes::Node.include ActiveRecord::Originator::ArelNodeExtension
+Arel::Nodes::Node.descendants.each do |klass|
+  klass.prepend ActiveRecord::Originator::ArelNodeExtension
+end
 Arel::Visitors::ToSql.prepend ActiveRecord::Originator::ArelVisitorExtension
 
 module ActiveRecord
